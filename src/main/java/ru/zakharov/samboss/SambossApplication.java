6d475f60963bc1468aa5a33bcf2212b41ef2c21f
lexaz777 <vh1@mail.ru>
2018-10-29 20:29:14 +0300

    Spring boot project init

---
+++ src/main/java/ru/zakharov/samboss/SambossApplication.java
@@ -0,0 +1,12 @@
+package ru.zakharov.samboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambossApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/zakharov/samboss/SambossApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.zakharov.samboss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SambossApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
