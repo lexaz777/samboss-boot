1a27ac9cdf445aa6075a580aad5d0e441c661364
lexaz777 <vh1@mail.ru>
2018-10-31 22:09:02 +0300

    Валидация почты

--- src/main/java/ru/zakharov/samboss/validation/ValidEmail.java
+++ src/main/java/ru/zakharov/samboss/validation/ValidEmail.java
@@ -1,4 +1,19 @@
 package ru.zakharov.samboss.validation;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEmail {
+    String message() default "Invalid email";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
